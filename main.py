9b10bdf770401f061c038008da4ead08b14fbb13
Roadcrosser <roadcrosser0@gmail.com>
2021-01-03 13:49:00 +0800

    add error handling

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from twitchAPI.oauth import UserAuthenticator
 from uuid import UUID
 
 import asyncio
+import traceback
 import colorsys
 import json
+import sys
 import re
 import os
 
@@ -75,102 +77,116 @@ twitch.session = None
 
 
 def callback(uuid: UUID, data: dict) -> None:
-    if DEBUG:
-        print("Data received: ", data)
-
-    ### This block for testing on whispers ###
-    if WHISPER_MODE:
-        if data["type"] != "whisper_received":
-            return
-
-        resp_data = data["data"]
-        resp_data = json.loads(resp_data)  # Comment this if it breaks somehow
-
-        initiating_user = resp_data["tags"]["login"]
-        original_color = resp_data["body"]
-
-    ### End Block ###
-
-    ### This block for prod channel points ###
-    else:
-        if data["type"] != "reward-redeemed":
-            return
-
-        resp_data = data["data"]
-        resp_data = json.loads(resp_data)  # Comment this if it breaks somehow
-
-        if resp_data["reward"]["title"] != REWARD_NAME:
-            return
-
-        initiating_user = resp_data["user"]["login"]
-        original_color = resp_data["user_input"]
-
-    ### End Block ###
-
-    if DEBUG:
-        print("User input: ", original_color)
-
-    original_color = re.sub(r"\s", "", str(original_color).strip())
-
-    color = original_color.lower().strip("#")
-
-    hue = 0
-    sat = 0
-    bri = 0
-
     try:
-        color = COLOR_LOOKUP.get(color, color)
-        color = re.sub(r"[^0-9a-f]", "0", "{:0>6}".format(color)[:6])
-        hue, sat, bri = colorsys.rgb_to_hsv(
-            int(color[:2], 16), int(color[2:4], 16), int(color[4:6], 16)
+        if DEBUG:
+            print("Data received: ", data)
+
+        ### This block for testing on whispers ###
+        if WHISPER_MODE:
+            if data["type"] != "whisper_received":
+                return
+
+            resp_data = data["data"]
+            resp_data = json.loads(resp_data)  # Comment this if it breaks somehow
+
+            initiating_user = resp_data["tags"]["login"]
+            original_color = resp_data["body"]
+
+        ### End Block ###
+
+        ### This block for prod channel points ###
+        else:
+            if data["type"] != "reward-redeemed":
+                return
+
+            resp_data = data["data"]
+            resp_data = json.loads(resp_data)  # Comment this if it breaks somehow
+
+            if resp_data["reward"]["title"] != REWARD_NAME:
+                return
+
+            initiating_user = resp_data["user"]["login"]
+            original_color = resp_data["user_input"]
+
+        ### End Block ###
+
+        if DEBUG:
+            print("User input: ", original_color)
+
+        original_color = re.sub(r"\s", "", str(original_color).strip())
+
+        color = original_color.lower().strip("#")
+
+        hue = 0
+        sat = 0
+        bri = 0
+
+        try:
+            color = COLOR_LOOKUP.get(color, color)
+            color = re.sub(r"[^0-9a-f]", "0", "{:0>6}".format(color)[:6])
+            hue, sat, bri = colorsys.rgb_to_hsv(
+                int(color[:2], 16), int(color[2:4], 16), int(color[4:6], 16)
+            )
+
+            # Hue: [0, 1) to [0, 360)
+            hue = int(hue * 360)
+            # Sat: [0, 1] to [0, 255]
+            sat = int(sat * 255)
+            # Bri: [0, 255] to [0, 255] (We're also ensuring this value stays within its bound
+            min_bri = MINIMUM_BRIGHTNESS / 100 * 255
+            max_bri = MAXIMUM_BRIGHTNESS / 100 * 255
+            bri = min(max(bri, min_bri), max_bri)
+
+        except:
+            print(f"{initiating_user}: Failed to parse color {original_color}")
+            return
+
+        if DEBUG:
+            print("Final color:", color)
+
+        payload = {"hue": hue, "sat": sat, "bri": bri}
+
+        if FORCE_ON:
+            payload["on"] = True
+
+        hue_id = HUE_ID
+
+        if DEBUG:
+            print("Sending to hue:", payload)
+
+        asyncio.get_event_loop().create_task(
+            callback_task(initiating_user, hue_id, payload, color)
         )
-
-        # Hue: [0, 1) to [0, 360)
-        hue = int(hue * 360)
-        # Sat: [0, 1] to [0, 255]
-        sat = int(sat * 255)
-        # Bri: [0, 255] to [0, 255] (We're also ensuring this value stays within its bound
-        min_bri = MINIMUM_BRIGHTNESS / 100 * 255
-        max_bri = MAXIMUM_BRIGHTNESS / 100 * 255
-        bri = min(max(bri, min_bri), max_bri)
-
-    except:
-        print(f"{initiating_user}: Failed to parse color {original_color}")
-        return
-
-    if DEBUG:
-        print("Final color:", color)
-
-    payload = {"hue": hue, "sat": sat, "bri": bri}
-
-    if FORCE_ON:
-        payload["on"] = True
-
-    hue_id = HUE_ID
-
-    if DEBUG:
-        print("Sending to hue:", payload)
-
-    asyncio.get_event_loop().create_task(
-        callback_task(initiating_user, hue_id, payload, color)
-    )
+    except Exception as e:
+        print(
+            "".join(traceback.TracebackException.from_exception(e).format()),
+            file=sys.stderr,
+        )
+        pass
 
 
 async def callback_task(initiating_user, bulb_id, payload, color):
+    try:
+        if DEBUG:
+            print("Running callback task...")
 
-    if DEBUG:
-        print("Running callback task...")
+        if not twitch.session:
+            twitch.session = aiohttp.ClientSession()
 
-    if not twitch.session:
-        twitch.session = aiohttp.ClientSession()
+        await twitch.session.put(
+            f"{HUE_URL}/api/{HUE_KEY}/lights/{bulb_id}/state",
+            headers=headers,
+            data=json.dumps(payload),
+        )
 
-    await twitch.session.put(
-        f"{HUE_URL}/api/{HUE_KEY}/lights/{bulb_id}/state",
-        headers=headers,
-        data=json.dumps(payload),
-    )
+        print(f"{initiating_user}: Changed bulb {bulb_id} color to #{color}")
 
-    print(f"{initiating_user}: Changed bulb {bulb_id} color to #{color}")
+    except Exception as e:
+        print(
+            "".join(traceback.TracebackException.from_exception(e).format()),
+            file=sys.stderr,
+        )
+        pass
 
 
 while not HUE_KEY:
